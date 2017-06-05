dc426db4d8055ffdff9fedc2d8f40cff6efcbc8a
Jimmy <jimmyroman@me.com>
2017-06-05 11:10:30 -0400

    upload files

---
+++ js/audioAssets.js
@@ -0,0 +1,11 @@
+'use strict';
+
+// Audio Source - WVMX-FM Live Stream : http://playerservices.streamtheworld.com/pls/WVMXFM.pls
+var audioData = [
+    {
+        'title' : 'Mix 107.9 Live Stream',
+        'url' : 'http://playerservices.streamtheworld.com/pls/WVMXFM.pls'
+    }
+];
+
+module.exports = audioData;
